c5df06bc9486f690df6e64636cddd6831ae000e0
darmariduan <dharmarhiduwhan@gmail.com>
2022-07-14 15:24:33 +0700

    route & view

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        'name' => 'Darma Riduan',
+        'email' => 'dharmarhiduwhan@gmail.com',
+        'image' => 'github.png'
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
