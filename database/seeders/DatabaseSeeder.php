8bf957be0342b522eed025e2d025ba698b552f93
darmariduan <dharmarhiduwhan@gmail.com>
2022-07-17 15:57:54 +0700

    Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +23,49 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name' => 'Darma Riduan',
+            'email' => 'freehost@gmail.com',
+            'password' =>bcrypt('password')
+        ]);
+
+        User::create([
+            'name' => 'Sativa',
+            'email' => 'sativa@gmail.com',
+            'password' =>bcrypt('password')
+        ]);
+
+        Category::create([
+            'name' => 'Web',
+            'slug' => 'web'
+        ]);
+
+        Category::create([
+            'name' => 'Network',
+            'slug' => 'network'
+        ]);
+
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' =>'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus corrupti odio quas voluptatem aliquam adipisci nemo quidem necessitatibus',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus corrupti odio quas voluptatem aliquam adipisci nemo quidem necessitatibus. Sit autem quaerat dicta distinctio molestiae quod a optio necessitatibus nostrum mollitia harum pariatur cupiditate esse, perspiciatis sapiente sunt officia delectus velit excepturi laborum voluptatum! Iure iste voluptas rerum eum illum eius!</p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus corrupti odio quas voluptatem aliquam adipisci nemo quidem necessitatibus. Sit autem quaerat dicta distinctio molestiae quod a optio necessitatibus nostrum mollitia harum pariatur cupiditate esse, perspiciatis sapiente sunt officia delectus velit excepturi laborum voluptatum! Iure iste voluptas rerum eum illum eius!</p>',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' =>'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus corrupti odio quas voluptatem aliquam adipisci nemo quidem necessitatibus',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus corrupti odio quas voluptatem aliquam adipisci nemo quidem necessitatibus. Sit autem quaerat dicta distinctio molestiae quod a optio necessitatibus nostrum mollitia harum pariatur cupiditate esse, perspiciatis sapiente sunt officia delectus velit excepturi laborum voluptatum! Iure iste voluptas rerum eum illum eius!</p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus corrupti odio quas voluptatem aliquam adipisci nemo quidem necessitatibus. Sit autem quaerat dicta distinctio molestiae quod a optio necessitatibus nostrum mollitia harum pariatur cupiditate esse, perspiciatis sapiente sunt officia delectus velit excepturi laborum voluptatum! Iure iste voluptas rerum eum illum eius!</p>',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
+
+
+
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,10 +4,10 @@
 <article>
 <h1 class="mb-5">{{ $post->title }}</h1>
 
-<p>By. Darma Riduan <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+<p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 {!! $post->body !!}
 </article>
 
-<a href="/posts">Back to Posts</a>
+<a href="/posts" class="d-block mt-3">Back to Posts</a>
 @endsection
 
