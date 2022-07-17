29ca92615ba8485fa7af752c69c44d9d0168b4b5
darmariduan <dharmarhiduwhan@gmail.com>
2022-07-17 10:33:46 +0700

    Post Model Binding

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,41 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-
-class Post
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            'title' => 'Judul Pertama',
-            'slug' => 'judul-pertama',
-            'author' => 'Darma',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure id doloribus nisi quia eius. Optio, eos architecto. Animi dignissimos modi eius quam reiciendis officiis impedit iusto possimus tempore dolore fuga voluptas expedita architecto necessitatibus veniam vel, dolorum vitae illo. Nesciunt, consequuntur obcaecati nostrum deserunt ea eveniet accusantium! Tenetur consequatur et cumque nam qui ratione, sapiente repudiandae quia, numquam magnam pariatur commodi aliquid quos blanditiis autem? Assumenda dicta deleniti repellat autem.',
-        ],
-        [
-            'title' => 'Judul Kedua',
-            'slug' => 'judul-kedua',
-            'author' => 'Darma',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure id doloribus nisi quia eius. Optio, eos architecto. Animi dignissimos modi eius quam reiciendis officiis impedit iusto possimus tempore dolore fuga voluptas expedita architecto necessitatibus veniam vel, dolorum vitae illo. Nesciunt, consequuntur obcaecati nostrum deserunt ea eveniet accusantium! Tenetur consequatur et cumque nam qui ratione, sapiente repudiandae quia, numquam magnam pariatur commodi aliquid quos blanditiis autem? Assumenda dicta deleniti repellat autem.',
-        ],
-        [
-            'title' => 'Judul Ketiga',
-            'slug' => 'judul-ketiga',
-            'author' => 'Darma',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure id doloribus nisi quia eius. Optio, eos architecto. Animi dignissimos modi eius quam reiciendis officiis impedit iusto possimus tempore dolore fuga voluptas expedita architecto necessitatibus veniam vel, dolorum vitae illo. Nesciunt, consequuntur obcaecati nostrum deserunt ea eveniet accusantium! Tenetur consequatur et cumque nam qui ratione, sapiente repudiandae quia, numquam magnam pariatur commodi aliquid quos blanditiis autem? Assumenda dicta deleniti repellat autem.',
-        ],
+    use HasFactory;
+    // required data
+    //protected $fillable = ['title', 'excerpt', 'body'];
 
-    ];
-
-    public static function all()
-    {
-        return collect(self::$blog_posts);
-    }
-
-    public static function find($slug)
-    {
-        $posts = static::all();
-        
-        return $posts->firstWhere('slug', $slug);
-    }
+    //no change
+    protected $guarded = ['id'];
 }
