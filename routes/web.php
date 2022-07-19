c18939d8fc685bbfb14c118c72b43bd9fd6d1a76
darmariduan <dharmarhiduwhan@gmail.com>
2022-07-20 03:04:47 +0700

    N+1 Problem

--- routes/web.php
+++ routes/web.php
@@ -37,25 +37,26 @@ Route::get('/about', function () {
 Route::get('/posts', [PostController::class, 'index']);
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
-Route::get('/categories', function(){
+Route::get('/categories', function () {
     return view('categories', [
         'title' => 'Post Categories',
         'categories' => Category::all()
 
     ]);
-
 });
 
-Route::get('categories/{category:slug}', function(Category $category) {
-    return view('category', [
-    'title' => $category->name,
-    'posts' => $category->posts,
-    'category' => $category->name]);
-});
-
-Route::get('/authors/{author:username}', function(User $author) {
+Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'title' => 'User Posts',
-        'posts' => $author->posts
-       ]);
+        //Lazy Eager Loading
+        'title' => "Post by Category: $category->name",
+        'posts' => $category->posts->load('category', 'author')
+    ]);
+});
+
+Route::get('/authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'title' => "Post by Author : $author->name",
+        //Lazy Eager Loading
+        'posts' => $author->posts->load('category', 'author')
+    ]);
 });
