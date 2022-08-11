cb879dbcbc17748fd9dc3e9c0ec668eb82dad658
darmariduan <dharmarhiduwhan@gmail.com>
2022-08-11 11:37:44 +0700

    Authorization

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use App\Models\User;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,7 +26,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
         Paginator::useBootstrapFour();
+
+        Gate::define('admin', function(User $user){
+            return $user->is_admin;
+        });
     }
 }
